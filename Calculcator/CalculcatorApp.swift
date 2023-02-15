f28e485184cf81fae1fe41a1ca3c55f8b56b7cfa
Naeema Muhammad <naeemamuhammad13@gmail.com>
2023-02-15 08:48:03 -0500

    Initial Commit

---
+++ Calculcator/CalculcatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculcatorApp.swift
+//  Calculcator
+//
+//  Created by Naeema Muhammad on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculcatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
